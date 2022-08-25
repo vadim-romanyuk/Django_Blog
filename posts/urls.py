cffbeed9dbd219f37ebbfd35cd06cae8bb99efe5
v.romanyuk <v.romanyuk@borosp.by>
2022-08-25 17:03:56 +0300

    add  user_post.html

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,9 @@ from posts import views as posts_views
 
 
 urlpatterns = [
+    path('', posts_views.show_posts),
     path('about/', posts_views.about, name='url_to_about'),
-    # path('create', posts_views.create_post, name='create_post'),
-    path('<str:post_id>', posts_views.posts),
-    path('create/', posts_views.CreateView.as_view(), name='url_to_create_post'),
+    path('create', posts_views.create_post, name='url_to_create_post'),
+    path('<post_id>', posts_views.post, name='get_post'),
+    # path('create/', posts_views.CreateView.as_view(), name='url_to_create_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from posts.forms import CreateForm
 from django.views import View
+from posts import models
 
 # Create your views here.
 
@@ -10,16 +11,14 @@ def home(request):
     return render(request, 'posts/base.html')
 
 
-def posts(request, post_id: str = None):
-    if post_id is None:
-        return render(request, 'posts/posts_1.html', {'content': '<h1>Posts</h1>'})
+def show_posts(request, post_id: str = None):
+    posts_ = models.Post.objects.all()
+    return render(request, 'posts/posts.html', {'posts': posts_})
 
-    if request.method == 'GET':
-        print("Параметры", request.GET)
-    # elif request.method == 'POST':
-    #     print("Параметры", request.POST)
-    post_content = request.headers
-    return render(request, 'posts/posts.html', {'content': request.GET})
+
+def post(request, post_id):
+    user_post = models.Post.objects.get(id=post_id)
+    return render(request, 'posts/user_post.html', {'post': user_post})
 
 
 def about(request):
@@ -28,19 +27,19 @@ def about(request):
 # def create(request):
 #     return render(request, 'posts/create.html', {'content': '<h1>Create</h1>'})
 
-def create_post(request):
-    print('create')
-    user_form = CreateForm()
-
-    if request.method == 'GET':
-        return render(request, 'posts/create.html', {'form': user_form, 'qwert': 'asdfdsad'})
-    elif request.method == 'POST':
-        user_form = CreateForm(request.POST)
-        if user_form.is_valid():
-            return redirect('/')
-        return render(request, 'posts/create.html', {'form': user_form})
-
-    return render(request, 'posts/create.html', {'form': user_form})
+# def create_post(request):
+#     print('create')
+#     user_form = CreateForm()
+#
+#     if request.method == 'GET':
+#         return render(request, 'posts/create.html', {'form': user_form, 'qwert': 'asdfdsad'})
+#     elif request.method == 'POST':
+#         user_form = CreateForm(request.POST)
+#         if user_form.is_valid():
+#             return redirect('/')
+#         return render(request, 'posts/create.html', {'form': user_form})
+#
+#     return render(request, 'posts/create.html', {'form': user_form})
 
 
 class CreateView(View):
@@ -60,3 +59,24 @@ class CreateView(View):
             return render(request, 'posts/create.html', {'form': user_form})
 
         return render(request, 'posts/create.html', {'form': user_form})
+
+
+def create_post(request):
+    print('CREATE FUNC')
+
+    if request.method == 'GET':
+        return render(request, 'posts/create.html')
+
+    elif request.method == 'POST':
+        print(request.POST)
+        title = request.POST.get('title') or ''
+        content = request.POST.get('content') or ''
+        if title and content:
+            # user_post = models.Post(title=request.POST['title'], content=request.POST['content'])
+            # user_post.save()
+            models.Post.objects.create(title=title, content=content)
+            return redirect('/')
+        else:
+            error = 'Укажите все поля'
+            return render(request, 'posts/create.html', {'title': title, 'content': content, 'error': error})
+
